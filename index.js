157b54715f044543af4edb8cc8c1390cb830ab84
Sangam Neela <neelasangam8433@gmail.com>
2024-12-09 22:05:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('sangam');
+console.log("rahul");
+
+function sayName(){
+    console.log("my name is ...")
+}
---
+++ src/hello.js
@@ -0,0 +1,4 @@
+function sayHello(){
+    return "hello man";
+}
+console.log("nithin");
